d68d376eab58cffd6be6019c196f2560efd4f280
Trent Willis <trentmwillis@gmail.com>
2022-12-29 16:41:38 -0500

    feat: first 20 album reviews

---
+++ albums.js
@@ -0,0 +1,463 @@
+export default [
+  {
+    artist: "ANORAK!",
+    album: "ANORAK!",
+    song: "吉祥寺 (Kichijoji)",
+    genre: ["emo", "j-rock"],
+    review: "When I lived in Tokyo for part of a summer, my train often passed Nakano and Kichijoji amongst other stops, so when my friend turned me on to this album I was already inclined to like it just based on the song names.<br><br>While not my favorite emo release of the year, <em>ANORAK!</em> has some memorable, mathy riffs and is a surprisingly solid and straightforward album for a band steeped in Tokyo culture.",
+    id: "anorak",
+    youtubeId: "0oPDMIJ_kKU"
+  },
+  {
+    artist: "Fleshwater",
+    album: "We're Not Here to Be Loved",
+    song: "The Razor's Apple",
+    genre: ["alternative metal", "shoegaze"],
+    review: "I am not a huge fan of Deftones-esque alt-metal, but every now and then I like a song or two in the genre. Freshwater's debut LP is probably the first full album that broke that trend. It has the perfect balance of dreamy atmosphere, punishing riffs, and aggression. Not necessarily a ground-breaking album, but one that I found myself listening to over and over again.",
+    id: "fleshwater",
+    youtubeId: "Omul2aBWWKs",
+    emphasized: true
+  },
+  {
+    artist: "Rottengraffty",
+    album: "Hello",
+    song: "Goodbye to Romance",
+    genre: ["j-rock"],
+    review: "Typically, I dislike genres like “J-Rock” because it reveals little about the sound of the band other than the country of origin. In Rottengraffty's case, I feel it is appropriate. The rock and metal band has such a variety over the course of <em>Hello</em> that it is hard to pin down a specific sub-genre. “Goodbye to Romance” is one of their softer songs and it hits right in the feels.",
+    id: "rottengraffty",
+    youtubeId: "KXVuRfMqhb8"
+  },
+  {
+    artist: "Show Me The Body",
+    album: "Trouble The Water",
+    song: "We Came To Play",
+    genre: ["hardcore", "noise", "punk"],
+    review: "Show Me The Body’s banjo-led, noise-influenced brand of hardcore doesn’t always land for me, but when it does, I’m all in. They’re one of those rare bands where you can tell they stay true to their values, in their lyrics, their music, and the community they’ve fostered. “We Came To Play” showcases all of that while delivering catchy riffs to boot.",
+    id: "show-me-the-body",
+    youtubeId: "qO2pLviiTi8"
+  },
+  {
+    artist: "Dayseeker",
+    album: "Dark Sun",
+    song: "Without Me",
+    genre: ["post-hardcore"],
+    review: "Rory, the lead singer of Dayseeker, is one of my favorite vocalists in the genre as of late. Leaning more into pop-vocal stylings and ambient soundscapes than most of their contemporaries, <em>Dark Sun</em> should be right up there with Bad Omens' <em><a href=\"#bad-omens\">TDOPOM</a></em> getting radio play and attention for the scene (at least in my humble opinion).",
+    id: "dayseeker",
+    youtubeId: "2ZtfU8eRENw"
+  },
+  {
+    artist: "Ben Quad",
+    album: "I'm Scared That's All There Is",
+    song: "We're Gonna Be Here for a While",
+    genre: ["emo", "pop punk"],
+    review: "Take the twinkly riffs of emo with the rhythmic sensabilities of pop punk and you'll get something close to Ben Quad. One of the shorter releases on this list, this album is a fun listen from start to finish.",
+    id: "ben-quad",
+    youtubeId: "9YEfrdTo16g"
+  },
+  {
+    artist: "Polyphia",
+    album: "Remember That You Will Die",
+    song: "ABC (feat. Sophia Black)",
+    genre: ["progressive metal", "pop"],
+    review: "Probably the most influential band in the world of \"guitar music\" right now, it seems like every time Polyphia drops a new song guitarists everywhere lose their minds. And, it is all reasonable in my opinion.<br><br>Tim Henson and crew have breathed new life into the scene by pushing the boundaries of progressive metal and rock beyond shredding virtuosity and into musical realms shared with pop and hip-hop. Put less pretentiously, they make pop music metalheads seem to like.",
+    id: "polyphia",
+    youtubeId: "Mtd24QIBJ5Y"
+  },
+  {
+    artist: "Varials",
+    album: "Scars For You To Remember",
+    song: "A Body Wrapped In Plastic : Prologue",
+    genre: ["metalcore"],
+    review: "Sounding a lot more polished than their early days, Varials is still a punishingly heavy band with groovy riffs. With this being their first album since the departure of their vocalist, I was skeptical but pleasantly surprised.<br><br>The new vocalist shares a similar vocal range and fits the band's sound in a way that sounds less like a band member change and more like an evolution. Anyway, this song in particular slaps.",
+    id: "varials",
+    youtubeId: "iWJiGv2JYhI"
+  },
+  {
+    artist: "Boston Manor",
+    album: "Datura",
+    song: "Foxglove",
+    genre: ["alternative rock", "pop punk (jk)"],
+    review: "Boston Manor is a band that I've followed since their semi-early days as a pop punk group. They've come a long way since then. No longer sounding anything like pop punk but leaning into elements of darker pop and alt rock music. Not the type of stuff I normally listen to, but they do it well (and put on a great live show).",
+    id: "boston-manor",
+    youtubeId: "JUpS2L-7RLg"
+  },
+  {
+    artist: "Survive Said The Prophet",
+    album: "Hateful Failures",
+    song: "Win / Lose",
+    genre: ["alternative rock", "easycore"],
+    review: "Another band that is hard to classify. <em>Hateful Failures</em> is largely an alt rock album, yet songs like \"Win / Lose\" have heavier elements that are more reminiscent of easycore or pop punk. Regardless, the band makes catchy choruses and evokes the same sense of fun that I get from my favorite pop punk groups.<br><br>(P.S., not off this album, but \"<a href=\"https://youtu.be/5DjFpPSuGR0\" target=\"_blank\" rel=\"noreferrer noopener\">Mukanjyo</a>\" is an absolute banger that you should check out.)",
+    id: "survive-said-the-prophet",
+    youtubeId: "BUFiuDW0XDI"
+  },
+  {
+    artist: "Counterparts",
+    album: "A Eulogy for Those Still Here",
+    song: "Whispers of Your Death",
+    genre: ["melodic hardcore"],
+    review: "An unexpectedly emotional release for the metalcore world, this song was written about the passing of the cat of vocalist Brendan. It may sound a bit cheesy, but in a scene filled with tough-guy posturing, it was remarkably refreshing to me. Not to mention, Counterparts still bring lovely guitar riffs and heavy breakdowns.",
+    id: "counterparts",
+    youtubeId: "N4Mca6dUtxc"
+  },
+  {
+    artist: "Origami Angel",
+    album: "DEPART",
+    song: "FOE",
+    genre: ["hardcore?"],
+    review: "Over the past year, I've really fallen for Origami Angel. They were my most palyed artist on my Spotify Wrapped even though they are predominantly a \"not heavy\" band. So when they released a surprise \"heavy\" EP, <em>DEPART</em> was something I immediately enjoyed.<br><br>They don't really sound like a hardcore band, but there are still moshable elements and it's different from the dominant sound of today's hardcore scene. All-in-all, a fun release for my inner \"hardcore kid\".",
+    id: "origami-angel-depart",
+    youtubeId: "xjuBmwt7TLE"
+  },
+  {
+    artist: "Within Destruction",
+    album: "Lotus",
+    song: "Nightmare",
+    genre: ["deathcore", "sad boi"],
+    review: "Within Destruction's last album, <em>Yokai</em> was a surprise for me. I'm not typically a deathcore fan, but their fusion of trap and deathcore appealed to me as someone that grew up in the age of nu metal. <em>Lotus</em> continues and, in some ways, improves on that previous effort.<br><br>Some would describe it as cheesy, and sometimes it is, but overall I find it to be a fun and creative twist in a genre I typically find lacking in sonic diversity.",
+    id: "within-destruction",
+    youtubeId: "m2cSy-PCt5U"
+  },
+  {
+    artist: "Origami Angel",
+    album: "re: turn",
+    song: "live from the ufo",
+    genre: ["emo", "acoustic"],
+    review: "Unlike <a href=\"#origami-angel-depart\"><em>DEPART</em></a>, <em>re: turn</em> is softer than Origami Angel's usual fare. While it isn't their strongest material by any means, it does play to their strengths in fun lyrics that you can't help but sing along to after the initial listen.<br><br>(It also vaguely reminds me of Relient K in a positive way.)",
+    id: "origami-angel-return",
+    youtubeId: "eDyUJpk6Ktg"
+  },
+  {
+    artist: "Novelists FR",
+    album: "Déjà Vu",
+    song: "Do you really wanna know?",
+    genre: ["metalcore"],
+    review: "After the release of the singles for this album, I was eagerly awaiting its release...but I was disappointed. The singles are by far the strongest songs on the album without there really being any other highlights. That said, this song and \"Lost Cause\" are certified bangers and thus 2022 wouldn't be complete for me without mentioning them.",
+    id: "novelists",
+    youtubeId: "GG-in8b2ALA"
+  },
+  {
+    artist: "Regulate",
+    album: "Regulate",
+    song: "In the Moment",
+    genre: ["hardcore"],
+    review: "Turnstile has brought mainstream attention to the hardcore scene, so it is expected that any notable hardcore release would draw comparisons to them. Regulate deserves such comparisons on their self-titled release, but they are also very different. \"In The Moment\" definitely has some of the catchy-chorus, power-chord-driven hardcore that Turnstile has refined, but they also leave their own unique mark. A band to watch in the coming years.",
+    id: "regulate",
+    youtubeId: "Wpvk_FdBzxo"
+  },
+  {
+    artist: "SiM",
+    album: "Beware",
+    song: "Light It Up",
+    genre: ["alternative metal", "reggae"],
+    review: "SiM is a band with a very distinct sound. They've evolved somewhat over the course of their career, but with every release that core, reggae punk sound driven by MAH's vocals is still present.<br><br>This year, they saw international attention from \"The Rumbling\" which was used as the opening song for the final season of Attack on Titan. <em>Beware</em> features that song along with 3 others and is a great representation of SiM's overall sound. I hope they continue to gain more international attention so I don't have to travel to Japan to see them again.",
+    id: "sim",
+    youtubeId: "p4HrKwZ035M"
+  },
+  {
+    artist: "Stray From The Path",
+    album: "Euthanasia",
+    song: "III",
+    genre: ["metalcore", "nu metal"],
+    review: "No stranger to hot button topics, Stray From The Path is back again with another album that has some bangers with unmissable sociopolitical themes. \"III\" is probably the most lyrically divisive song on this list. It continues the critique of policing in the United States that the previous two \"Badge & A Bullet\" tracks began.<br><br>Whether you agree with the lyrics or not, there is unmistakable grooviness to this latest track and album; something that STFP have honed over their many years as a band.",
+    id: "stray-from-the-path",
+    youtubeId: "2rlIav2RSD0"
+  },
+  {
+    artist: "The Devil Wears Prada",
+    album: "Color Decay",
+    song: "Salt",
+    genre: ["metalcore"],
+    review: "I've enjoyed every album The Devil Wears Prada have ever released and <em>Color Decary</em> is no exception. They continue to push the boundaries of metalcore in a way that I feel is both artistic and accessible (though some of my friends disagree and feel they're too \"avant garde\"). I've appreciated seeing the band mature and evolve into a distinct sound with totally abandoning their roots as a metalcore band.<br><br>\"Salt\" in particular is probably the most radio-friendly they've ever sounded and yet it doesn't feel out of place even amongst their heavier songs.",
+    id: "the-devil-wears-prada",
+    youtubeId: "9ldQAuWzIxY"
+  },
+  {
+    artist: "PUP",
+    album: "THE UNRAVELING OF PUPTHEBAND",
+    song: "PUPTHEBAND Inc. Is Filing For Bankruptcy",
+    genre: ["punk", "emo"],
+    review: "PUP, for me, perfectly captures the satirical angst about society's current state for most millenials. And this song, easily PUP's most aggressive, puts that front and center. That said, this album and <em>Morbid Stuff</em> are fun listens all the way through; they take punk music but make it more accessible with a little emo lyricism and twinkly guitars. Oh, and they put on a hell of a good live show (even in the rain).",
+    id: "pup",
+    youtubeId: "g8m1mWqDqVY"
+  },
+
+
+  {
+    artist: "",
+    album: "",
+    song: "",
+    genre: [],
+    review: "",
+    id: "",
+    youtubeId: ""
+  },
+  {
+    artist: "Mindforce",
+    album: "",
+    song: "",
+    genre: [],
+    review: "",
+    id: "",
+    youtubeId: "",
+    emphasized: true
+  },
+  {
+    artist: "",
+    album: "",
+    song: "",
+    genre: [],
+    review: "",
+    id: "",
+    youtubeId: ""
+  },
+  {
+    artist: "",
+    album: "",
+    song: "",
+    genre: [],
+    review: "",
+    id: "",
+    youtubeId: ""
+  },
+  {
+    artist: "Creepy Nuts",
+    album: "",
+    song: "",
+    genre: [],
+    review: "",
+    id: "",
+    youtubeId: "",
+    emphasized: true
+  },
+  {
+    artist: "",
+    album: "",
+    song: "",
+    genre: [],
+    review: "",
+    id: "",
+    youtubeId: ""
+  },
+  {
+    artist: "",
+    album: "",
+    song: "",
+    genre: [],
+    review: "",
+    id: "",
+    youtubeId: ""
+  },
+  {
+    artist: "",
+    album: "",
+    song: "",
+    genre: [],
+    review: "",
+    id: "",
+    youtubeId: ""
+  },
+  {
+    artist: "",
+    album: "",
+    song: "",
+    genre: [],
+    review: "",
+    id: "",
+    youtubeId: ""
+  },
+  {
+    artist: "",
+    album: "",
+    song: "",
+    genre: [],
+    review: "",
+    id: "",
+    youtubeId: ""
+  },
+  {
+    artist: "",
+    album: "",
+    song: "",
+    genre: [],
+    review: "",
+    id: "",
+    youtubeId: ""
+  },
+  {
+    artist: "",
+    album: "",
+    song: "",
+    genre: [],
+    review: "",
+    id: "",
+    youtubeId: ""
+  },
+  {
+    artist: "Pool Kids",
+    album: "Pool Kids",
+    song: "",
+    genre: [],
+    review: "",
+    id: "",
+    youtubeId: "",
+    emphasized: true
+  },
+  {
+    artist: "Asunojokei",
+    album: "",
+    song: "",
+    genre: [],
+    review: "",
+    id: "",
+    youtubeId: "",
+    emphasized: true
+  },
+  {
+    artist: "",
+    album: "",
+    song: "",
+    genre: [],
+    review: "",
+    id: "",
+    youtubeId: ""
+  },
+  {
+    artist: "",
+    album: "",
+    song: "",
+    genre: [],
+    review: "",
+    id: "",
+    youtubeId: ""
+  },
+  {
+    artist: "Camp Trash",
+    album: "",
+    song: "",
+    genre: [],
+    review: "",
+    id: "",
+    youtubeId: "",
+    emphasized: true
+  },
+  {
+    artist: "",
+    album: "",
+    song: "",
+    genre: [],
+    review: "",
+    id: "",
+    youtubeId: ""
+  },
+  {
+    artist: "Earthists.",
+    album: "",
+    song: "",
+    genre: [],
+    review: "",
+    id: "",
+    youtubeId: "",
+    emphasized: true
+  },
+  {
+    artist: "Gridiron",
+    album: "",
+    song: "",
+    genre: [],
+    review: "",
+    id: "",
+    youtubeId: "",
+    emphasized: true
+  },
+  {
+    artist: "Bad Omens",
+    album: "",
+    song: "",
+    genre: [],
+    review: "",
+    id: "",
+    youtubeId: "",
+    emphasized: true
+  },
+  {
+    artist: "",
+    album: "",
+    song: "",
+    genre: [],
+    review: "",
+    id: "",
+    youtubeId: ""
+  },
+  {
+    artist: "",
+    album: "",
+    song: "",
+    genre: [],
+    review: "",
+    id: "",
+    youtubeId: ""
+  },
+  {
+    artist: "",
+    album: "",
+    song: "",
+    genre: [],
+    review: "",
+    id: "",
+    youtubeId: ""
+  },
+  {
+    artist: "",
+    album: "",
+    song: "",
+    genre: [],
+    review: "",
+    id: "",
+    youtubeId: ""
+  },
+  {
+    artist: "",
+    album: "",
+    song: "",
+    genre: [],
+    review: "",
+    id: "",
+    youtubeId: ""
+  },
+  {
+    artist: "",
+    album: "",
+    song: "",
+    genre: [],
+    review: "",
+    id: "",
+    youtubeId: ""
+  },
+  {
+    artist: "",
+    album: "",
+    song: "",
+    genre: [],
+    review: "",
+    id: "",
+    youtubeId: ""
+  },
+  {
+    artist: "",
+    album: "",
+    song: "",
+    genre: [],
+    review: "",
+    id: "",
+    youtubeId: ""
+  },
+  {
+    artist: "",
+    album: "",
+    song: "",
+    genre: [],
+    review: "",
+    id: "",
+    youtubeId: ""
+  }
+];
