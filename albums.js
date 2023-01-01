f7179c4872b4a8349e9529a76f6e030eddd8ee86
Trent Willis <trentmwillis@gmail.com>
2022-12-31 20:22:55 -0500

    feat: initial UI design

--- albums.js
+++ albums.js
@@ -46,6 +46,16 @@ export default [
     youtubeId: "qO2pLviiTi8"
   },
   {
+    artist: "Mindforce",
+    album: "New Lords",
+    song: "Survival Is Vengeance",
+    genre: ["hardcore", "thrash"],
+    review: "Thrash-y hardcore is seeing somewhat of a revival as of late and Mindforce is one of the standard bearers for the movement. <em>New Lords</em> was not an album I expected to like and yet I've listened to it more-or-less on repeat since it was released. The songs are short and to the point, yet filled with catchy hooks, groovy riffs, and rhythm that makes me want to mosh. Plus, Jay's voice lends a unique flair to their sound.",
+    id: "mindforce",
+    youtubeId: "W_aTt6DSYuM",
+    emphasized: true
+  },
+  {
     artist: "Dayseeker",
     album: "Dark Sun",
     song: "Without Me",
@@ -80,6 +90,16 @@ export default [
     review: "Sounding a lot more polished than their early days, Varials is still a punishingly heavy band with groovy riffs. With this being their first album since the departure of their vocalist, I was skeptical but pleasantly surprised.<br><br>The new vocalist shares a similar vocal range and fits the band's sound in a way that sounds less like a band member change and more like an evolution. Anyway, this song in particular slaps.",
     id: "varials",
     youtubeId: "iWJiGv2JYhI"
+  },
+  {
+    artist: "Creepy Nuts",
+    album: "アンサンブル-プレイ (Ensemble Play)",
+    song: "2way nice guy",
+    genre: ["hip-hop", "indie"],
+    review: "Jazzy, upbeat, and catchy hip-hop is how I would describe Creepy Nuts' sound. The Japanese duo have a sound that is quite unlike anything else I've heard; you can tell they incorporate influences from a variety of genres into something new and fun. It's not for everyone, but I'd recommend giving it a chance.",
+    id: "creepy-nuts",
+    youtubeId: "iZtDuxKztgA",
+    emphasized: true
   },
   {
     artist: "Boston Manor",
@@ -118,6 +138,16 @@ export default [
     youtubeId: "xjuBmwt7TLE"
   },
   {
+    artist: "Pool Kids",
+    album: "Pool Kids",
+    song: "Arm's Length",
+    genre: ["emo"],
+    review: "This album took me by surprise. I knew nothing about Pool Kids when they were announced on the <a href=\"#origami-angel-depart\">Origami Angel</a> tour this past fall, so I checked them out and was quickly captivated by this album. After seeing them live, its place as one of my favorites of the year was solidified.<br><br>This is an emo album with both atmosphere and catchy hooks. Maybe it's the subtle way it sounds like old-school Paramore, but this album gives me a sense of nostalgia while at the same time feeling very current and timely. Anyway, I dig it.",
+    id: "pool-kids",
+    youtubeId: "ayB8Whgkg_E",
+    emphasized: true
+  },
+  {
     artist: "Within Destruction",
     album: "Lotus",
     song: "Nightmare",
@@ -152,6 +182,16 @@ export default [
     review: "Turnstile has brought mainstream attention to the hardcore scene, so it is expected that any notable hardcore release would draw comparisons to them. Regulate deserves such comparisons on their self-titled release, but they are also very different. \"In The Moment\" definitely has some of the catchy-chorus, power-chord-driven hardcore that Turnstile has refined, but they also leave their own unique mark. A band to watch in the coming years.",
     id: "regulate",
     youtubeId: "Wpvk_FdBzxo"
+  },
+  {
+    artist: "Asunojokei (明日の叙景)",
+    album: "Island",
+    song: "Chimera",
+    genre: ["black metal", "shoegaze"],
+    review: "As I said for <a href=\"#holy-fawn\">Holy Fawn</a>, anything with shoegaze is not usually one of my preferred genres; takes too long to get where it's going in my opinion. Asunojokei, however, make the long journey worth it. <em>Island</em> and \"Chimera\" in particular are filled with unique and interesting twists and variations throughout. As one co-worker put it, it is \"somehow really catchy\".",
+    id: "asunojokei",
+    youtubeId: "IsVyACY3QDw",
+    emphasized: true
   },
   {
     artist: "SiM",
@@ -190,6 +230,16 @@ export default [
     youtubeId: "g8m1mWqDqVY"
   },
   {
+    artist: "coldrain",
+    album: "Nonnegative",
+    song: "Cut Me",
+    genre: ["alternative metal"],
+    review: "It's no shocker that I've long preferred coldrain's heavier side. Because of that, their last album wasn't one of my favorites, but <em>Nonnegative</em> was a return to form while still keeping some tracks that embraced their lighter side. Their blend of hard rock with metalcore always stands out in my music rotation; it's more radio-friendly while still being heavy.",
+    id: "coldrain",
+    youtubeId: "v_qzxGf7_xo",
+    emphasized: true
+  },
+  {
     artist: "Stick To Your Guns",
     album: "Spectre",
     song: "More of Us Than Them",
@@ -197,16 +247,6 @@ export default [
     review: "\"Anthemic\" is the first word that comes to mind when I think about Stick To Your Guns. Their particular brand of hardcore from Orange County is heavy and yet they have catchy chorus that feel like anthems for the scene. Overall, <em>Spectre</em> is not my favorite work from them, but \"More of Us Than Them\" definitely slaps.",
     id: "stick-to-your-guns",
     youtubeId: "ja7x7UXclb8"
-  },
-  {
-    artist: "Mindforce",
-    album: "New Lords",
-    song: "Survival Is Vengeance",
-    genre: ["hardcore", "thrash"],
-    review: "Thrash-y hardcore is seeing somewhat of a revival as of late and Mindforce is one of the standard bearers for the movement. <em>New Lords</em> was not an album I expected to like and yet I've listened to it more-or-less on repeat since it was released. The songs are short and to the point, yet filled with catchy hooks, groovy riffs, and rhythm that makes me want to mosh. Plus, Jay's voice lends a unique flair to their sound.",
-    id: "mindforce",
-    youtubeId: "W_aTt6DSYuM",
-    emphasized: true
   },
   {
     artist: "Chat Pile",
@@ -227,16 +267,6 @@ export default [
     youtubeId: "4L5FpH0lRT0"
   },
   {
-    artist: "Creepy Nuts",
-    album: "アンサンブル-プレイ (Ensemble Play)",
-    song: "2way nice guy",
-    genre: ["hip-hop", "indie"],
-    review: "Jazzy, upbeat, and catchy hip-hop is how I would describe Creepy Nuts' sound. The Japanese duo have a sound that is quite unlike anything else I've heard; you can tell they incorporate influences from a variety of genres into something new and fun. It's not for everyone, but I'd recommend giving it a chance.",
-    id: "creepy-nuts",
-    youtubeId: "iZtDuxKztgA",
-    emphasized: true
-  },
-  {
     artist: "JYOCHO",
     album: "Let's Promise to Be Happy",
     song: "Gather the Lights",
@@ -244,6 +274,16 @@ export default [
     review: "Take folksy indie music and sprinkle in some mathy, twinkly guitar riffs and you'll have an approximation of JYOCHO. <em>Let's Promise to Be Happy</em> is such an interesting album to me; it is equal parts calming, soothing, and engaging. Not typically in the realm of stuff I would normally listen to but I enjoy it.",
     id: "jyocho",
     youtubeId: "bJiqGdQrZL0"
+  },
+  {
+    artist: "Camp Trash",
+    album: "The Long Way, The Slow Way",
+    song: "Weird Florida",
+    genre: ["emo", "indie"],
+    review: "This album was the soundtrack to my summer. Plenty of catchy hooks and upbeat vibes while still being emo at its core. Also helps that the band is from Florida and I got to see them live at the start of summer.",
+    id: "camp-trash",
+    youtubeId: "kkTqwcCsyaM",
+    emphasized: true
   },
   {
     artist: "downt",
@@ -282,6 +322,16 @@ export default [
     youtubeId: "q7HncSyfV1w"
   },
   {
+    artist: "Earthists.",
+    album: "Have A Good Cult",
+    song: "Lost Grace",
+    genre: ["progressive metalcore"],
+    review: "Generally, I dislike \"progressive\" music which is just complex and technical instrumentality. But, \"progressive\" music which actually attempts to move a genre forward by incorporating other influences is endlessly interesting to me. <em>Have A Good Cult</em> is such an album. The piano-driven \"Lost Grace\" is one of the first singles I heard from this album and it is unlike any other metalcore song I've heard and in a good way.",
+    id: "earthists",
+    youtubeId: "PPATGjgahTc",
+    emphasized: true
+  },
+  {
     artist: "Unprocessed",
     album: "Gold",
     song: "Closer",
@@ -289,26 +339,6 @@ export default [
     review: "Easily my most anticipated album of the year. Formerly a djent band, Unprocessed have reinvented themselves as a progressive sort of rock band with pop influences. In some ways similar to <a href=\"#polyphia\">Polyphia</a> but in many ways their own, they have managed to craft a sound that appeals to the metalhead, guitarist, and melody lover in me. In short, I think most people could find at least one song of <em>Gold</em> that they enjoy.",
     id: "unprocessed",
     youtubeId: "XvHQDcpaM8Q"
-  },
-  {
-    artist: "Pool Kids",
-    album: "Pool Kids",
-    song: "Arm's Length",
-    genre: ["emo"],
-    review: "This album took me by surprise. I knew nothing about Pool Kids when they were announced on the <a href=\"#origami-angel-depart\">Origami Angel</a> tour this past fall, so I checked them out and was quickly captivated by this album. After seeing them live, its place as one of my favorites of the year was solidified.<br><br>This is an emo album with both atmosphere and catchy hooks. Maybe it's the subtle way it sounds like old-school Paramore, but this album gives me a sense of nostalgia while at the same time feeling very current and timely. Anyway, I dig it.",
-    id: "pool-kids",
-    youtubeId: "ayB8Whgkg_E",
-    emphasized: true
-  },
-  {
-    artist: "Asunojokei (明日の叙景)",
-    album: "Island",
-    song: "Chimera",
-    genre: ["black metal", "shoegaze"],
-    review: "As I said for <a href=\"#holy-fawn\">Holy Fawn</a>, anything with shoegaze is not usually one of my preferred genres; takes too long to get where it's going in my opinion. Asunojokei, however, make the long journey worth it. <em>Island</em> and \"Chimera\" in particular are filled with unique and interesting twists and variations throughout. As one co-worker put it, it is \"somehow really catchy\".",
-    id: "asunojokei",
-    youtubeId: "IsVyACY3QDw",
-    emphasized: true
   },
   {
     artist: "World of Pleasure",
@@ -320,25 +350,6 @@ export default [
     youtubeId: "apGgnc34Ang"
   },
   {
-    artist: "coldrain",
-    album: "Nonnegative",
-    song: "Cut Me",
-    genre: ["alternative metal"],
-    review: "It's no shocker that I've long preferred coldrain's heavier side. Because of that, their last album wasn't one of my favorites, but <em>Nonnegative</em> was a return to form while still keeping some tracks that embraced their lighter side. Their blend of hard rock with metalcore always stands out in my music rotation; it's more radio-friendly while still being heavy.",
-    id: "coldrain",
-    youtubeId: "v_qzxGf7_xo"
-  },
-  {
-    artist: "Camp Trash",
-    album: "The Long Way, The Slow Way",
-    song: "Weird Florida",
-    genre: ["emo", "indie"],
-    review: "This album was the soundtrack to my summer. Plenty of catchy hooks and upbeat vibes while still being emo at its core. Also helps that the band is from Florida and I got to see them live at the start of summer.",
-    id: "camp-trash",
-    youtubeId: "kkTqwcCsyaM",
-    emphasized: true
-  },
-  {
     artist: "Speed",
     album: "Gang Called Speed",
     song: "Not That Nice",
@@ -348,14 +359,13 @@ export default [
     youtubeId: "EfCROIuI9VE"
   },
   {
-    artist: "Earthists.",
-    album: "Have A Good Cult",
-    song: "Lost Grace",
-    genre: ["progressive metalcore"],
-    review: "Generally, I dislike \"progressive\" music which is just complex and technical instrumentality. But, \"progressive\" music which actually attempts to move a genre forward by incorporating other influences is endlessly interesting to me. <em>Have A Good Cult</em> is such an album. The piano-driven \"Lost Grace\" is one of the first singles I heard from this album and it is unlike any other metalcore song I've heard and in a good way.",
-    id: "earthists",
-    youtubeId: "PPATGjgahTc",
-    emphasized: true
+    artist: "No Pressure",
+    album: "No Pressure",
+    song: "Too Far",
+    genre: ["pop punk"],
+    review: "This album came out of nowhere for me. I had never heard of No Pressure, but on release day so many bands I follow were posting about it, and for good reason. <em>No Pressure</em> leans more on the punk than the pop, but still is super fun to listen to. Nothing really new, just really solid pop punk.",
+    id: "no-pressure",
+    youtubeId: "jZw9L3P7p24"
   },
   {
     artist: "Gridiron",
@@ -366,25 +376,6 @@ export default [
     id: "gridiron",
     youtubeId: "Sp26zoWMYeo",
     emphasized: true
-  },
-  {
-    artist: "Bad Omens",
-    album: "The Death of Peace of Mind",
-    song: "Just Pretend",
-    genre: ["metalcore", "post-hardcore", "alternative metal"],
-    review: "Bad Omens blew up this year and it was well deserved. <em>TDOPOM</em> is probably one my favorite albums of recent years and I could rave about it for paragraphs but, in short, it has everything I love about music in this scene. Give \"Just Pretend\" a listen.",
-    id: "bad-omens",
-    youtubeId: "ekU1dQjMsOQ",
-    emphasized: true
-  },
-  {
-    artist: "No Pressure",
-    album: "No Pressure",
-    song: "Too Far",
-    genre: ["pop punk"],
-    review: "This album came out of nowhere for me. I had never heard of No Pressure, but on release day so many bands I follow were posting about it, and for good reason. <em>No Pressure</em> leans more on the punk than the pop, but still is super fun to listen to. Nothing really new, just really solid pop punk.",
-    id: "no-pressure",
-    youtubeId: "jZw9L3P7p24"
   },
   {
     artist: "malevolence",
@@ -423,6 +414,16 @@ export default [
     youtubeId: "Y_ne93vKAt0"
   },
   {
+    artist: "Bad Omens",
+    album: "The Death of Peace of Mind",
+    song: "Just Pretend",
+    genre: ["metalcore", "post-hardcore", "alternative metal"],
+    review: "Bad Omens blew up this year and it was well deserved. <em>TDOPOM</em> is probably one my favorite albums of recent years and I could rave about it for paragraphs but, in short, it has everything I love about music in this scene. Give \"Just Pretend\" a listen.",
+    id: "bad-omens",
+    youtubeId: "ekU1dQjMsOQ",
+    emphasized: true
+  },
+  {
     artist: "Kublai Khan TX",
     album: "Lowest Form of Animal",
     song: "Swan Song",
@@ -458,4 +459,4 @@ export default [
     id: "underoath",
     youtubeId: "PEn_Fb4IPA4"
   }
-];
+].reverse();
